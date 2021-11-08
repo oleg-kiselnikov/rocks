3cd12237585935e5cde2cb205d9f4197a00be6eb
Oleg Kiselnikov <oleg.kiselnikov@yandex.ru>
2021-11-08 04:37:59 +0300

    ...

--- src/components/crag-list/crag-list.js
+++ src/components/crag-list/crag-list.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import CragListItem from '../crag-list-item';
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { withService } from '../hoc';
+import { cragsLoaded } from '../../actions'
 import './crag-list.css';
 
 class CragList extends Component {
+
+    componentDidMount() {
+        const { service } = this.props;
+        const data = service.getCrags();
+        this.props.cragsLoaded(data);
+    }
+
     render() {
         const { crags } = this.props;
 
@@ -21,4 +31,17 @@ class CragList extends Component {
     }
 }
 
-export default CragList;
+const mapStateToProps = ({ crags }) => {
+    return { crags };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    
+    return {
+        cragsLoaded: (newCrags) => {
+            dispatch(cragsLoaded(newCrags))
+        }
+    };
+};
+
+export default withService()(connect(mapStateToProps, mapDispatchToProps)(CragList));
