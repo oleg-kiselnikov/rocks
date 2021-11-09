52bf0c7b903b95e3b175cc2ff75d7b2df53fd38c
Oleg Kiselnikov <oleg.kiselnikov@yandex.ru>
2021-11-10 01:37:51 +0300

    ...

--- src/store.js
+++ src/store.js
@@ -2,6 +2,23 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const originalDispatch = store.dispatch;
+    store.dispatch = (action) => {
+
+        if (typeof action === 'string') {
+            return originalDispatch({
+                type: action
+            });
+        }
+
+        return originalDispatch(action);
+    }
+
+    return store;
+}
+
+const store = createStore(reducer, enhancer);
 
 export default store;
